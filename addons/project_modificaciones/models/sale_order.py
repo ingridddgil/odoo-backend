70f6ab41a866791cd71a72cc028631772a4bcb96
ingriddd <ingrid.sglh@gmail.com>
2025-10-14 13:10:17 -0600

    Refactorización del archivo sale_order.py (clases: OrderIncident-OrderSource-OrderDestination) (variables: sproject_id, service_status, source_id, destination_id, incident_id)

--- addons/project_modificaciones/models/sale_order.py
+++ addons/project_modificaciones/models/sale_order.py
@@ -3,7 +3,7 @@ from odoo import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     
-    sproject_id = fields.Many2one('project.project', 'Proyecto', domain="[('sale_order_id.id', '=', id)]")
+    project_id = fields.Many2one('project.project', 'Proyecto', domain="[('sale_order_id.id', '=', id)]")
     project_progress_ids = fields.One2many('project.sub.update', 'sale_order_id', string='Avances del Proyecto')
 
     service_status = fields.Selection(
